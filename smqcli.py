3c932baa978699484a91227735ec1eaefdabecbd
dvinesett <dvinesett@gmail.com>
2015-11-23 18:07:14 -0500

    added url to output array, added TODOs

--- smqcli.py
+++ smqcli.py
@@ -3,19 +3,23 @@ import re
 import requests
 import time
 
+# TODO: cli input: choose between raw sequence, sequence in fasta file, sequence from ncib accession number (gi number works too)
+# TODO: decide the best way to structure cli option formatting
+# TODO: translate nucleotides to protien (all 6 frames) for querying similar to tblastn
+# TODO: output option for saving results file
+# TODO: output option for displaying results, output delimiter
+
 def motif_to_regex(raw_motif):
     motif = re.sub('[Xx]', '.', raw_motif)
     return re.compile(motif, re.IGNORECASE)
 
 motifs = ['VXV','VXXV'] # arg
-accessions = ['ALJ99974'] # arg
+accessions = ['ALJ99974', 'AIU94630'] # arg
 sequences = {}
 matches = []
-#sequence = 'aaattatagggatatata'
 
 # TODO: validate motif
 
-#matches = {re.compile(motif): None for motif in motifs} # initialize keys for dict with empty value
 compiled_motifs = [re.compile(motif) for motif in motifs]
 
 print('Will wait 0.5s between each search so entrez server doesn\'t get angry')
@@ -28,24 +32,19 @@ for an in accessions:
     split = request.text.split('\n', 1) # split on fist newline
     sequences[an] = split[1].replace('\n', '')
 
-    print("[%s/%s] ... "%(count_accessions, num_accessions))
-    time.sleep(0.5)
-print('done querying web')
-
-for an in sequences:
-    #for match in list(matches):#.finditer(sequence): # list(matches) are the keys of dict
     for motif in motifs:
         for hit in motif_to_regex(motif).finditer(sequences[an]):
-            #print("%s:\t%s\t%s" % (an, hit.span(), hit.group(0)))
-            matches.append([an, motif, hit])
-            #matches[match.span()] = match.group(0)
+            matches.append([an, motif, hit, url]) # TODO: replace 'matches' with object
+
+    print("[%s/%s] %s "%(count_accessions, num_accessions, an))
+    time.sleep(0.5)
+    count_accessions += 1
+
 print('done looking for hits')
 
 print()
 for match in matches:
-    print("%s\t%s\t%s\t%s" % (match[0], match[1], match[2].span(), match[2].group(0)))
+    print("%s,%s,%s,%s,%s" % (match[0], match[1], match[2].span(), match[2].group(0), match[3]))
 
-#for match in match.finditer(sequence):
-#    matches[match.span()] = match.group(0)
 
 # http://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?sendto=on&dopt=fasta&val=940373999
